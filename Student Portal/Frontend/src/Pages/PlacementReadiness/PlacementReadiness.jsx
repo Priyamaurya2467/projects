88ad99750075b174ecf0306880012d164391416f
Priya Maurya <priyamaurya1612@gmail.com>
2026-06-08 11:44:28 +0530

    update

--- Student Portal/Frontend/src/Pages/PlacementReadiness/PlacementReadiness.jsx
+++ Student Portal/Frontend/src/Pages/PlacementReadiness/PlacementReadiness.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { HiOutlinePencil } from 'react-icons/hi'
 import { HiMiniClipboardDocument } from 'react-icons/hi2'
-import { MdArrowLeft, MdKeyboardArrowLeft, MdOutlineArrowLeft, MdPeople, MdTerminal } from 'react-icons/md'
+import { MdArrowLeft, MdCheckCircle, MdKeyboardArrowLeft, MdOutlineArrowLeft, MdOutlineError, MdPeople, MdSyncLock, MdTerminal, MdThunderstorm, MdTrendingUp } from 'react-icons/md'
+import { FaBrain } from "react-icons/fa6";
 
 function PlacementReadiness() {
     const tracker = [
@@ -25,12 +26,75 @@ function PlacementReadiness() {
 
         }
     ]
+    const skills = [
+        {
+            icon: <MdTerminal size={24}/>,
+            topic: "DSA" ,
+            status: "85%",
+            caption: "✓ Excellent"
+        },
+        {
+            icon: <FaBrain size={24} /> ,
+            topic: "Aptitude" ,
+            status: "70%",
+            caption: "○ Good"
+
+        },
+        {
+            icon: <MdPeople size={24} /> ,
+            topic: "Communication" ,
+            status: "75%",
+            caption: "○ Good"
+
+        },
+        {
+            icon: <HiMiniClipboardDocument size={24} /> ,
+            topic: "Mock Interviews" ,
+            status: "60%",
+            caption: "△ Fair"
+
+        }
+    ]
+    const roadmap = [
+        {
+            icon: <MdCheckCircle/>,
+            topic: "Resume Built",
+            status: 'Completed',
+            progress: 'Done'
+
+        },
+        {
+            icon: <MdCheckCircle/>,
+            topic: "Portfolio Setup",
+            status: 'Completed',
+            progress: 'Done'
+
+        },
+        {
+            icon: <MdSyncLock/>,
+            topic: "Skill Assessment",
+            status: 'In Progress',
+            progress: 'In Progress'
+        },
+        {
+            icon: <MdOutlineError/>,
+            topic: "Interview Prep",
+            status: 'Pending',
+            progress: ''
+        },
+        {
+            icon: <MdOutlineError/>,
+            topic: "Job Applications",
+            status: 'Pending',
+            progress: ''
+        }
+    ]
   return (
-    <>
+    <div>
 
     {/* Navbar */}
 
-    <div className='bg-gradient-to-r from-purple-50 to-purple-100 min-h-screen'>
+    <div className='bg-gradient-to-r from-purple-50 to-purple-100 min-h-screen '>
         <div className='flex justify-between p-5 bg-white '>
             <div className='flex justify-center items-center gap-2'><MdOutlineArrowLeft/> Placement Readiness</div>
             <div>
@@ -69,16 +133,84 @@ function PlacementReadiness() {
                 <span className='text-xs text-gray-600'>2 completed, 3 remaining</span>
             </div>
         </div>
-    </div>
+    
 
-    {/* Skills Progress */}
+        {/* Skills Progress */}
 
-    <div>
-        
+        <div className="m-4">
+  <h1 className="flex items-center text-3xl p-4 gap-2 tracking-wider">
+    <MdTrendingUp className="text-purple-900" />
+    Skills Progress
+  </h1>
+
+  {skills.map((skill, index) => (
+    <div
+      key={index}
+      className="flex items-center justify-between bg-white p-6 rounded-xl shadow-sm hover:shadow-md transition mb-4"
+    >
+      <div className="flex items-center gap-4">
+        <div className="h-12 w-12 bg-purple-100 flex items-center justify-center text-purple-950 rounded-xl">
+          {skill.icon}
+        </div>
+
+        <div>
+          <h3 className="font-semibold text-gray-800 text-xl">
+            {skill.topic}
+          </h3>
+          <p className="text-xs text-gray-600 font-medium">
+            {skill.caption}
+          </p>
+        </div>
+      </div>
+
+      <div className="text-lg font-medium">
+        {skill.status}
+      </div>
     </div>
+  ))}
+        </div>
+
+
+        <div className="m-4">
+  <h1 className="flex items-center text-3xl p-4 gap-2 tracking-wider">
+    <MdThunderstorm className="text-purple-900" />
+    Placement Roadmap
+  </h1>
+
+  {roadmap.map((mapping, index) => (
+    <div
+      key={index}
+      className="flex items-center justify-between bg-white p-6 rounded-xl shadow-sm hover:shadow-md transition mb-4"
+    >
+      <div className="flex items-center gap-4">
+        <div className="h-12 w-12 bg-purple-100 flex items-center justify-center text-purple-950 text-2xl  rounded-xl">
+          {mapping.icon}
+        </div>
+
+        <div>
+          <h3 className="font-semibold text-gray-800 text-xl">
+            {mapping.topic}
+          </h3>
+          <p className="text-xs text-gray-600 font-medium">
+            {mapping.status}
+          </p>
+        </div>
+      </div>
+
+      <div className="text-lg font-medium">
+        {mapping.progress}
+      </div>
+    </div>
+  ))}
+        </div>
+
+
+
+    </div>
+  
     
     
-    </>
+    </div>
   )
 }
 
